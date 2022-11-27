93904b6beebd028510a2d0a1c87a5bc67a2da590
Chappo050 <matthewchaplin60@gmail.com>
2022-11-27 11:43:53 +1100

    graphQL JWT working

--- src/resolvers/mutations/user/createUser.ts
+++ src/resolvers/mutations/user/createUser.ts
@@ -1,42 +1,43 @@
-import bcrypt from 'bcryptjs';
-import {ApolloError} from "apollo-server-errors";
-const jwt = require('jsonwebtoken');
+import bcrypt from "bcryptjs";
+import { ApolloError } from "apollo-server-errors";
+const jwt = require("jsonwebtoken");
 
-module.exports = async (_: any, {input}: any, {models}: any) => {
-    let newUser: any
-    models.UserModel.findOne({username: input.username}).exec (
-        async (err: Error, userFound: any) => {
-            if (err) {
-                return (err)
-            }
-         if (userFound) {
-            throw new ApolloError("Username already exists", "USER_ALREADY_EXISTS" )
-         }
-         else {
-            //Hash password
-            const hashedPassword = await bcrypt.hash(input.password, 10); 
-           
-
-            //Create our JWT
-            const token = jwt.sign(
-                {username: input.username},
-                process.env.TOKEN_SECRET, {
-                    expiresIn: "2h"
-                }
-            )
-
-           return await models.UserModel.create({username: input.username, email:input.email.toLowerCase(), password: hashedPassword, token: token})
-      
-        
-
-         
-            
-         }
+module.exports = async (_: any, { input }: any, { models }: any) => {
+  return new Promise<void>((resolve, reject) => {
+    let newUser: any;
+    models.UserModel.findOne({ username: input.username }).exec(
+      async (err: Error, userFound: any) => {
+        if (err) {
+          return err;
         }
-    )
+        if (userFound) {
+          return reject(new ApolloError(
+            "Username already exists",
+            "USER_ALREADY_EXISTS"
+          ))
+        } else {
+          //Hash password
+          const hashedPassword = await bcrypt.hash(input.password, 10);
 
-    console.log(newUser);
-    
-return newUser
-   
-}
+          //Create our JWT
+          const token = jwt.sign(
+            { username: input.username },
+            process.env.TOKEN_SECRET,
+            {
+              expiresIn: "2h",
+            }
+          );
+
+          const newUser = await models.UserModel.create({
+            username: input.username,
+            email: input.email.toLowerCase(),
+            password: hashedPassword,
+            token: token,
+          });
+
+          resolve(newUser);
+        }
+      }
+    );
+  });
+};
--- src/resolvers/mutations/user/loginUser.ts
+++ src/resolvers/mutations/user/loginUser.ts
@@ -1,53 +1,43 @@
-import bcrypt from 'bcryptjs';
-import {ApolloError} from "apollo-server-errors";
-const jwt = require('jsonwebtoken');
-
+import bcrypt from "bcryptjs";
+import { ApolloError } from "apollo-server-errors";
+const jwt = require("jsonwebtoken");
 
 const { sign, verify } = jwt;
 module.exports = async (_: any, { input }: any, { models }: any) => {
-  let user: any;
-  models.UserModel.findOne({ username: input.username }).exec(
-    async (err: Error, userFound: any) => {
-      if (err) {
-        return err;
-      }
-      if (!userFound) {
-    
-        throw new ApolloError(
-          "Username cannot be found",
-          "USER_DOES_NOT_EXIST"
-        );
-      } else {
-        //Check password matches encrypted password
-        if (await bcrypt.compare(input.password, userFound.password)) {
-            
-          //Create new token
-          const token = jwt.sign(
-            
-            {username: input.username },
-            process.env.TOKEN_SECRET,
-            {
-              expiresIn: "2h",
-            }
+  return new Promise<void>((resolve, reject) => {
+    let user: any;
+    models.UserModel.findOne({ username: input.username }).exec(
+      async (err: Error, userFound: any) => {
+        if (err) {
+          reject(err);
+        }
+        if (!userFound) {
+          return reject(
+            new ApolloError("Username cannot be found", "USER_DOES_NOT_EXIST")
           );
+        } else {
+          //Check password matches encrypted password
+          if (await bcrypt.compare(input.password, userFound.password)) {
+            //Create new token
+            const token = jwt.sign(
+              { username: input.username },
+              process.env.TOKEN_SECRET,
+              {
+                expiresIn: "2h",
+              }
+            );
 
-          userFound.token = token
-          console.log(userFound);
-          return userFound
-          
-        }
-
-
-        else {
+            userFound.token = token;
+            console.log(userFound);
+            resolve(userFound);
+          } else {
             //Incorrect password
-            throw new ApolloError(
-                "Incorrect Password",
-                "WRONG_PASSWORD"
-              );
+            return reject(
+              new ApolloError("Incorrect Password", "WRONG_PASSWORD")
+            );
+          }
         }
       }
-    }
-  );
-
-
+    );
+  });
 };
